4037b78a4e5a26dda9b82c67e4ec21a27378b99a
dianamariarico <132197127+dianamariarico@users.noreply.github.com>
2024-10-23 09:23:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+javascript
+Copiar código
+document.getElementById('loginButton').addEventListener('click', function() {
+    alert('Iniciar sesión');
+});
+
+document.getElementById('signupButton').addEventListener('click', function() {
+    alert('Crear cuenta');
+});
